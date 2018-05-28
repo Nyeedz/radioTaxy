fc1f5762c59ecd089a617173ecda35d4e7568705
nyeedz <chr.alex@hotmail.com>
2018-05-28 15:22:05 -0300

    first commit

---
+++ android/app/src/main/java/com/radiotaxy/MainApplication.java
@@ -0,0 +1,31 @@
+package com.radiotaxy;
+
+import com.reactnativenavigation.NavigationApplication;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends NavigationApplication {
+    @Override
+    public boolean isDebug() {
+        return BuildConfig.DEBUG;
+    }
+
+    protected List<ReactPackage> getPackages() {
+        // Add additional packages you require here
+        // No need to add RnnPackage and MainReactPackage
+        return Arrays.<ReactPackage>asList(
+                // eg. new VectorIconsPackage()
+        );
+    }
+
+    @Override
+    public List<ReactPackage> createAdditionalReactPackages() {
+        return getPackages();
+    }
+
+    @Override
+    public String getJSMainModuleName() {
+        return "index";
+    }
+}
